f7b91b22d37c6ec1ecf339487aafd777b535f6ec
heinj <58248995+heinj@users.noreply.github.com>
2020-04-23 12:13:56 +0300

    juu

--- aboutUs.php
+++ aboutUs.php
@@ -8,22 +8,39 @@ include("includes/header.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/contacts.css">
+    <link rel="stylesheet" href="css/about.css">
     <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@500&family=Gotu&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Document</title>
 </head>
 <body>
     <header>
-        <h1>Tarinamme</h1>
+        <h1>Tietoa yrityksestä</h1>
     </header>
-    <section>
-        <h3>Mikä ihmeen Virtual PT - Senior Edition?</h3>
-        <p>
-            Oletko yli 55-vuotias? Haluatko pysyä terveenä ja toimintakykyisenä mahdollisimman pitkään tai saavuttaa hyvän olon eläkepäiviä viettäessäsi? Jos vastasit kyllä, olet tullut oikeaan osoitteseen. Meidän mottomme on "kohti terveempää ikääntymistä" ja haluamme auttaa sinua tavoitteissasi. 
-        </p>
-    </section>
-    <section>
+    <div class="about">
+        <h2>Meidän tarinamme</h2>
+            <p>
+                Olemme kolme hyvinvointi- ja terveysteknologian korkeakouluopiskelijaa Metropoliasta. Meille kaikille liikunta on iso osa elämää ja tiedämme sen tuomat hyödyt kaikille elämän osa-aluille. Hyvän kunnon saavuttaminen ei loppujen lopuksi katso juurikaan ikää, jonka vuoksi haluamme kannustaa ikääntyviä liikkumaan. Idea lähti siitä, kun keskustelimme omista isovanhemmistamme ja pohdimme, miksi useisiin vaivoihin (väestössä ylipäätään) on määrätty lääkitystä, vaikka oikeastaan liikuntasuunnitelman avulla olisi voitu saavuttaa yhtä hyviä, ellei jopa parempia tuloksia. Palvelumme käyttö ei lupaa ihmeparantumista tai yhtäkkistä huippukunnon saavuttamista, sillä muutos lähtee yksilöstä, sinusta, itsestään, mutta haluamme olla tukemassa matkaasi. Ymmärsimme, että voimme itse toteuttaa sovelluksen, mutta laadukkaan yksilöohjauksen toteuttaaksemme tarvitsimme ammattilaisia, jotka voisivat ohjata käyttäjiä (sinua) sovelluksen välityksellä. Siksi palkkasimmekin tiimimme mukaan fysioterapeutin, liikunnanohjaajan sekä ravitsemusterapeutin, jotka pitävät huolen siitä, että saat juuri sinulle suunniteltua ohjausta.
+            </p>
+        <h2>Miksi käyttää Virtual PT:tä</h2>
+            <p>
+                Oletko yli 55-vuotias? Haluatko pysyä terveenä ja toimintakykyisenä mahdollisimman pitkään tai saavuttaa hyvän olon eläkepäiviä viettäessäsi? Haluaisitko yksilöllistä ohjausta liikuntaan tai ravitsemukseen liittyen? Jos vastasit kyllä, olet tullut oikeaan osoitteseen. Meidän mottomme on "kohti terveempää ikääntymistä" ja haluamme auttaa sinua saavuttamaan tavoitteesi. 
+            </p>
+        <h2>Miten Virtual PT toimii?</h2>
+            <p>
+                1. Sinun tulee rekisteröityä sovelluksen käyttäjäksi ja tehdä haluamasi tilaus. Tilauksien hinnat ja niihin sisältyvät ominaisuudet on listattu alle.<br>
+            </p>
+                <p class="orders">
+                    a.	Silver: 9,95€/kk. Hintaan sisältyy kaikki sovelluksen käyttöön liittyvät ominaisuudet (omien tietojen kirjaaminen, päiväkirja, tietopankin sisällöt, valmiit treenisuunnitelmat), mutta ei sähköistä viestintää valmentajien kanssa.<br> 
+                    b.	Gold: 34,95€/kk. Hintaan sisältyy kaikki sovelluksen käyttöön liittyvät ominaisuudet (omien tietojen kirjaaminen, päiväkirja, tietopankin sisällöt, valmiit treenisuunnitelmat) sekä yhden valitun valmentajan kanssa henkilökohtainen ohjaus. Valmentajaa saa vaihtaa neljän viikon välein. Henkilökohtainen ohjaus keskittyy siihen, mihin valittu ammattilainen on erikoistunut. Ravitsemusterapeutit antavat ruokavalioon keskittyvää ohjausta, valmentajat liikuntaan ja treenaamiseen keskittyvää ohjausta ja fysioterapeutit antavat liikuntaan ohjausta niille, joilla on joitakin vammoja, joita haluavat kuntouttaa tai liikuntarajoitteita/niitä aiheuttavia tiloja kehossaan.<br>
+                    c.	Platinum: 59,95€/kk. Hintaan sisältyy kaikki sovelluksen käyttöön liittyvät ominaisuudet (omien tietojen kirjaaminen, päiväkirja, tietopankin sisällöt, valmiit treenisuunnitelmat) sekä kaikkien alojen ammattilaisten tarjoama henkilökohtainen konsultaatio samanaikaisesti. Valmentajaa saa vaihtaa neljän viikon välein. Valmentajakohtainen henkilökohtainen ohjaus keskittyy siihen, mihin valittu ammattilainen on erikoistunut. Ravitsemusterapeutit antavat ruokavalioon keskittyvää ohjausta, valmentajat liikuntaan ja treenaamiseen keskittyvää ohjausta ja fysioterapeutit antavat liikuntaan ohjausta niille, joilla on joitakin vammoja, joita haluavat kuntouttaa tai liikuntarajoitteita/niitä aiheuttavia tiloja kehossaan. Platinum asiakkaiden ohjaamisessa kaikki häntä ohjaavat ammattilaiset tekevät yhteistyötä, niin että ohjaus on sujuvaa sekä suunniteltu ravitsemusohjelma tukee treenisuunnitelmaa ja päinvastoin.<br><br>
+                </p>
+            <p>
+                2. Kirjaudu sovellukseen.<br>
+                3. Sovellus on nyt käytettävissäsi ja kaikki tietosisältö on luettavissasi.<br> 
+                4. Päiväkirja -sivulla voit täyttää omia terveystietoja ja kirjata ylös päiväsi kulun mm. tiedot liikkumisesta, nukkumisesta ja syödystä ruoasta.<br> 
+                5. Gold tai Platinum -asiakkaana voit valita ammattilaisen, joka tekee sinulle 4 viikon ohjelman ja seuraa edistymistäsi viikoittaisissa tarkistuksissa. 
+            </p>
         <h2>Omistajat</h2>
         <div class="boxPerson">
             <h3>Scrum Master</h3>
@@ -40,6 +57,6 @@ include("includes/header.php");
             <p>vastuualueet:<br><br><br><br><br>p.+358 40 5571241<br>henrikee@metropolia.fi</p>
             <img src="img/henkka.jpeg" alt="henkkaa">
         </div>
-</section>
+    </div>
 </body>
 </html>
