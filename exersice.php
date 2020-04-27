00a703402ca51f07baa4be7e8a2e7b39d4339018
heinj <58248995+heinj@users.noreply.github.com>
2020-04-27 18:15:32 +0300

    liikka

--- exersice.php
+++ exersice.php
@@ -17,16 +17,88 @@ include("includes/header.php");
 <span id="date"></span>
    <header>
        <h1>Liikunta ja harjoittelu</h1>
+       <h2>Etkö ole ihan varma miten kannattaisi liikkua?<br> Täältä löydät esimerkkiohjelmia ja vinkkejä, joita voit soveltaa omaan arkeesi!</h3>
    </header>
    <section>
-        <div class="card">
-            <p>aloittelija</p>
+   <div class="card">
+            <h3>Tehokas 30 minuutin seniorijumppa</h3>
+            <p>Liikkeet aktivoivat aivoja, tasapainoa ja kehoa juuri oikealla tavalla. Voit kerätä muutamia liikkeitä talteen ja tehdä niitä vaikka useaan kertaan päivässä.<br>
+            Jumppaan pääset painamalla <a href="https://www.youtube.com/watch?v=2HHFjquWri4">tästä!</a><br>
+            </p>
         </div>
         <div class="card">
-            <p>keskiverto</p>
+            <h3>Viikoksi terveysliikuntaa, sisältää seniorijumppia:</h3>
+            <p>Maanantai:<br>
+            20 minuuttia kävelyä omaan tahtiin + tuolijumppa. Voit jakaa liikunnan päivälle niin, että käyt esimerkiksi aamupäivällä kävelyllä ja iltapäivällä teet tuolijumpan.<br> 
+            TUOLIJUMPPA: tarvitset apuvälineeksi tuolin, jossa on selkänoja. Esimerkiksi keittiönpöydän tuoli sopii mainiosti. Voit tehdä liikkeet videon tahdissa tai laittaa halutessasi videon tauolle. 
+            Tuolijumppa löytyy painamalla <a href="https://www.youtube.com/watch?v=TcQmPZ2aSDo">tästä!</a><br><br>
+            Tiistai:<br>
+            Alle 30 minuutin jumppa.
+	        Jumppa löytyy painamalla <a href="https://www.youtube.com/watch?v=jTchifJdAPI">tästä!</a><br><br>
+            Keskiviikko:<br>	
+            Tasapainoharjoituksia. Pyri olemaan jokaisessa asennossa 10-50 sekuntia. Jos tasapaino välillä pettää, älä lannistu vaan hae vain asento uudelleen. Alkuun voit pitää nostettavan jalan varpaita hyvin lähellä lattiaa.<br>
+            <strong>-	Askel eteen ja takajalan ojennus taakse.</strong> Ota askel eteen ja ojenna kädet sivuille. Pidä hartiat alhaalla. Nosta taaempaa jalkaa suorana taakse. Tee molemmilla jaloilla.<br>
+            <img src="img/tasapaino1.png" alt="tasapaino1"><br>
+            <strong>-	Polvennosto. Ojenna kädet suoriksi sivuille.</strong> Keskity pitämään selkä suorana koko liikkeen ajan. Tee molemmilla jaloilla.
+            <img src="img/tasapaino2.png" alt="tasapaino2"><br>
+            <strong>-	Jalan avaus ja vienti ristiin.</strong> Pyri pitämään jalat mahdollisimman suorana. Tee liikettä 4-5 kertaa per jalka.<br>
+            <img src="img/tasapaino3.png" alt="tasapaino3"><br>
+            <strong>-	Viivalla kävely.</strong> Aseta esimerkiksi ohut lanka/teippiä lattialle tai ulkona piirrä suora viiva hiekkaan ja pyri kävelemään sen päästä päähän niin, että jalat pysyvät mahdollisimman tarkasti merkityn viivan päällä.<br>
+            Vinkki: Jos mahdollista, kävelylenkeillä kannattaa suunnata välillä metsäpolulle tai muulle epätasaiselle alustalle kävelemään, sillä sekin kehittää tasapainoa.<br><br>
+            Torstai:<br>	
+            20 minuuttia kävelyä omaan tahtiin + tuolijumppa. Voit jakaa liikunnan päivälle niin, että käyt esimerkiksi aamupäivällä kävelyllä ja iltapäivällä teet tuolijumpan.<br>
+            TUOLIJUMPPA: tarvitset apuvälineeksi tuolin, jossa on selkänoja. Esimerkiksi keittiönpöydän tuoli sopii mainiosti. Voit tehdä liikkeet videon tahdissa tai laittaa halutessasi videon tauolle.<br>
+            Tuolijumppa löytyy painamalla <a href="https://www.youtube.com/watch?v=o8N_NZwn5ps">tästä!</a><br><br>
+            Perjantai:<br> 	
+            20 minuuttia kävelyä + hartiajumppaa + venyttelyä.<br>
+            Käsijumppaliikkeet (voit ottaa halutessasi kevyttä painoa käsiin, esim. puolen litran vesipullot, jotka voit täyttää oman vastusmieltymyksen mukaan), voit tehdä enemmän kuin yhden kierroksen:<br>
+            <strong>-	Puristus.</strong><br>
+            Nosta kädet suoraan eteen. Purista käsiä tiukasti nyrkkiin noin 5 sekuntia ja harota sen jälkeen sormet niin levälleen kuin saat. Toista 10-15 kertaa.<br>
+            <strong>-	Hartiaseudun liikkeet.</strong><br>
+            1.	Seiso suorana, käsivarret rentoina sivuilla. Piirrä ilmaan olkapäilläsi vuorotellen numerot yhdestä kymmeneen, niin että molemmat olkapäät piirtävät kaikki numerot. Pyri pitämään muu keho mahdollisimman paikoillaan, paitsi piirtävä olkapää.<br>
+            2.	Nosta kädet suoraan eteenpäin ja piirrä nyt vuorotellen nyrkeilläsi numerot ilmaan. Voit pitää myös sormia suorana. Pidä aina toinen käsivarsi mahdollisimman paikoillaan eteenpäin ojennettuna, kun piirrät toisella.<br>
+            3.	Nosta kädet nyt suoraan ylöspäin ja toista numeroiden piirtämiset edelliseen tapaan.<br>
+            4.	Vie käsivarret suoriksi sivuille ja toista numeroiden piirtämiset edelliseen tapaan.<br>
+            Voit tehdä omat venyttelyt tai ottaa mallia <a href="https://www.youtube.com/watch?v=Lyu3PK3wzew">tästä!</a><br><br>
+            Lauantai:<br> 	
+            Lepopäivä. Rentoutumista.<br><br>
+            Sunnuntai:<br>
+            30-45 minuutin kävely- tai sauvakävelylenkki. Jos sinulla ei ole sauvoja, voit heilutella käsiä reippaasti sivuilla askeleiden tahdissa, kuvitellen, että sivulla olisi sauvat kädessä.<br>
+            </p>
         </div>
         <div class="card">
-            <p>iron man</p>
+            <h3>Viikoksi terveysliikuntaa, ei seniorijumppia:</h3>
+            <p>Maanantai:<br> 	
+            20 minuutin kävelylenkki + kuntopiiri. Tarvitset kellon, josta näet sekunnit.<br>
+	        KUNTOPIIRI: tee jokaista liikettä 45 sekuntia, jonka jälkeen 30 sekunnin tauko ennen kuin siirryt seuraavaan. Pyri tekemään ainakin kaksi kierrosta.<br>
+            -	Paikoillaan marssi tai juoksu. Nosta polvia mahdollisimman ylös.<br>
+            -	Kannat peppuun askellus tai juoksu. Tuo kantapäät joka askeleella niin lähelle takapuolta kuin mahdollista.<br> 
+            -	Askelkyykyt. Pidä kädet vyötäröllä ja selkä suorana. Otas askel taakse, takajalan päkiä lattiaan, pidä etummaisen jalan jalkapohja tukevasti paikoillaan ja kyykkää. Kyykätessä polven tulee pysyä keskimmäisten varpaiden kanssa samassa linjassa.<br>
+            -	Kyykyt. Pidä kädet edessäsi koukussa tai suoraksi ojennettuina sekä polvet keskimmäisten varpaiden kanssa samassa linjassa.<br>
+            -	Nelinkontin vastakkainen käsi ja jalka nousee. Ojenna samaan aikaan vastakkainen käsi ja vastakkainen jalka suoraksi vartalon jatkoksi. Pidä katse koko ajan lattiassa ja vedä vatsaa sisään.<br>
+            -	Punnerrukset. Voit laittaa polvet lattiaan halutessasi.<br>
+            -	Pyöräilyrutistukset. Asetu selinmakuulle, polvet koukussa ja kädet pään takana. Lähde viemään vastakkaista kyynärpäätä kohti vastakkaista polvea. Pyri pitämään niska suorana.<br><br>
+            Tiistai:<br>	
+            30-45 minuutin kävelylenkki + joogaa.<br>
+	        Jooga videoon pääset <a href="https://www.youtube.com/watch?v=HhT8PNErWnE">tästä!</a><br><br>
+            Keskiviikko:<br> 	
+            30 minuutin kevyt jumppa. Jumppa löytyy <a href="https://www.youtube.com/watch?v=3zOdbL52hjs">tästä!</a><br><br>
+            Torstai:<br>	
+            20 minuutin kävelylenkki + kuntopiiri. Tarvitset kellon, josta näet sekunnit.<br> 
+            -	Kuntopiiri: tee jokaista liikettä 45 sekuntia, jonka jälkeen 30 sekunnin tauko ennen kuin siirryt seuraavaan. Pyri tekemään ainakin kaksi kierrosta.<br> 
+            -	Paikoillaan marssi tai juoksu. Nosta polvia mahdollisimman ylös.<br>
+            -	Kannat peppuun askellus tai juoksu. Tuo kantapäät joka askeleella niin lähelle takapuolta kuin mahdollista.<br>
+            -	Kyykyt. Pidä kädet edessäsi koukussa tai suoraksi ojennettuina sekä polvet keskimmäisten varpaiden kanssa samassa linjassa.<br> 
+            -	Nelinkontin vastakkainen käsi ja jalka nousee. Ojenna samaan aikaan vastakkainen käsi ja vastakkainen jalka suoraksi vartalon jatkoksi. Pidä katse koko ajan lattiassa ja vedä vatsaa sisään.<br>
+            -	Punnerrukset. Voit laittaa polvet lattiaan halutessasi.<br>
+            -	Pyöräilyrutistukset. Asetu selinmakuulle, polvet koukussa ja kädet pään takana. Lähde viemään vastakkaista kyynärpäätä kohti vastakkaista polvea. Pyri pitämään niska suorana.<br><br>
+            Perjantai:<br> 	
+            30-45 minuutin kävelylenkki.<br><br>
+            Lauantai:<br> 	
+            Lepopäivä.<br><br>
+            Sunnuntai:<br>	
+            45min kävelyä tai pyöräilyä tai 30 min uintia + 20 min venyttelyä.<br> 
+            </p>
         </div>
     </section>
 </body>
